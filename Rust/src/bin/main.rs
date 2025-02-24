84ba8a0ed7abbbc725e11d90e3a35f5e181ea357
ChenDylanLiang <lcwater98@gmail.com>
2025-02-24 09:55:14 -0500

    make INPUT_DIM const for mcfwdad

--- Rust/src/bin/main.rs
+++ Rust/src/bin/main.rs
@@ -2,9 +2,11 @@ use ad_trait_eval::EvaluationConditionPack;
 use std::io;
 use std::time::Instant;
 use apollo_rust_linalg_adtrait::{ApolloDVectorTrait, V};
-fn benchmark(n: usize, m:usize, o:usize, passes:usize) -> [f64; 6] {
-    let mut durations = [0.0; 6];
-    let approaches = ["forward_ad", "reverse_ad", "mc8", "mc16","mc32", "mc64"];
+use ad_trait_eval::INPUT_DIM;
+
+fn benchmark(n: usize, m:usize, o:usize, passes:usize) -> [f64; 3] {
+    let mut durations = [0.0; 3];
+    let approaches = ["forward_ad", "reverse_ad", "mc_forward_ad",];
     for p in 0..passes {
         println!("Pass {p} running...");
         let pack = EvaluationConditionPack::new(n, m, o);
@@ -16,10 +18,7 @@ fn benchmark(n: usize, m:usize, o:usize, passes:usize) -> [f64; 6] {
             let (_, d_gt) = match approach {
                 "forward_ad" => pack.forward_ad.derivative(x_vec),
                 "reverse_ad" => pack.reverse_ad.derivative(x_vec),
-                "mc8" => pack.forward_ad_multi_8.derivative(x_vec),
-                "mc16" => pack.forward_ad_multi_16.derivative(x_vec),
-                "mc32" => pack.forward_ad_multi_32.derivative(x_vec),
-                "mc64" => pack.forward_ad_multi_64.derivative(x_vec),
+                "mc_forward_ad" => pack.mc_forward_ad.derivative(x_vec),
                 _ => panic!("Unknown approach: {}", approach),
             };
             let duration = start.elapsed().as_secs_f64()*1000.0*1000.0;
@@ -33,12 +32,12 @@ fn benchmark(n: usize, m:usize, o:usize, passes:usize) -> [f64; 6] {
     durations
 }
 fn main() {
-    println!("Please enter n, m, o, and passes");
+    println!("Please enter m, o, and passes");
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read");
     let nums: Vec<usize> = input
         .split_whitespace()
         .map(|s| s.parse().unwrap())
         .collect();
-    let durations=benchmark(nums[0], nums[1], nums[2], nums[3]);
+    let durations=benchmark(INPUT_DIM, nums[0], nums[1], nums[2]);
 }
--- Rust/src/lib.rs
+++ Rust/src/lib.rs
@@ -5,15 +5,12 @@ use ad_trait::differentiable_block::DifferentiableBlock;
 use ad_trait::differentiable_function::{ForwardAD, ForwardADMulti, ReverseAD};
 use ad_trait::forward_ad::adfn::adfn;
 use eval1::{BenchmarkFunctionNalgebra, DCBenchmarkFunctionNalgebra};
-
+pub const INPUT_DIM: usize = 10;
 pub struct EvaluationConditionPack {
     //pub finite_differencing: DifferentiableBlock<DifferentiableFunctionClassBenchmarkFunction2, FiniteDifferencing>,
     pub forward_ad: DifferentiableBlock<DCBenchmarkFunctionNalgebra, ForwardAD>,
     pub reverse_ad: DifferentiableBlock<DCBenchmarkFunctionNalgebra, ReverseAD>,
-    pub forward_ad_multi_8: DifferentiableBlock<DCBenchmarkFunctionNalgebra, ForwardADMulti<adfn<8>>>,
-    pub forward_ad_multi_16: DifferentiableBlock<DCBenchmarkFunctionNalgebra, ForwardADMulti<adfn<16>>>,
-    pub forward_ad_multi_32: DifferentiableBlock<DCBenchmarkFunctionNalgebra, ForwardADMulti<adfn<32>>>,
-    pub forward_ad_multi_64: DifferentiableBlock<DCBenchmarkFunctionNalgebra, ForwardADMulti<adfn<64>>>,
+    pub mc_forward_ad: DifferentiableBlock<DCBenchmarkFunctionNalgebra, ForwardADMulti<adfn<INPUT_DIM>>>,
 }
 impl EvaluationConditionPack {
     pub fn new(n: usize, m: usize, o: usize) -> Self {
@@ -22,10 +19,7 @@ impl EvaluationConditionPack {
             //finite_differencing: DifferentiableBlock::new_with_tag(DifferentiableFunctionClassBenchmarkFunction2, FiniteDifferencing::new(), f.clone(), f.clone()),
             forward_ad: DifferentiableBlock::new_with_tag(DCBenchmarkFunctionNalgebra, ForwardAD::new(), f.clone(), f.clone()),
             reverse_ad: DifferentiableBlock::new_with_tag(DCBenchmarkFunctionNalgebra, ReverseAD::new(), f.clone(), f.clone()),
-            forward_ad_multi_8: DifferentiableBlock::new_with_tag(DCBenchmarkFunctionNalgebra, ForwardADMulti::<adfn<8>>::new(), f.clone(), f.clone()),
-            forward_ad_multi_16: DifferentiableBlock::new_with_tag(DCBenchmarkFunctionNalgebra, ForwardADMulti::<adfn<16>>::new(), f.clone(), f.clone()),
-            forward_ad_multi_32: DifferentiableBlock::new_with_tag(DCBenchmarkFunctionNalgebra, ForwardADMulti::<adfn<32>>::new(), f.clone(), f.clone()),
-            forward_ad_multi_64: DifferentiableBlock::new_with_tag(DCBenchmarkFunctionNalgebra, ForwardADMulti::<adfn<64>>::new(), f.clone(), f.clone()),
+            mc_forward_ad: DifferentiableBlock::new_with_tag(DCBenchmarkFunctionNalgebra, ForwardADMulti::<adfn<INPUT_DIM>>::new(), f.clone(), f.clone()),
         }
     }
 }
